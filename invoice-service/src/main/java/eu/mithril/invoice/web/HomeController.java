4a856e1b8f2d93c4004ca009c9acc3f2f580579f
Marko Šutić <marko.sutic@mithril.eu>
2025-01-23 13:57:58 +0100

    launcher

--- invoice-service/src/main/java/eu/mithril/invoice/web/HomeController.java
+++ invoice-service/src/main/java/eu/mithril/invoice/web/HomeController.java
@@ -1,13 +1,22 @@
 package eu.mithril.invoice.web;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(
+            Model model,
+            @RequestParam("username") String username
+    ) {
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", LocalDateTime.now());
         return "index.html";
     }
 }
