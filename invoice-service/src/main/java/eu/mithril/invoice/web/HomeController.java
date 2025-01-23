6a944576597a72de86fdd0c3865ae3b7221358ac
Marko Šutić <marko.sutic@mithril.eu>
2025-01-23 14:14:10 +0100

    launcher

--- invoice-service/src/main/java/eu/mithril/invoice/web/HomeController.java
+++ invoice-service/src/main/java/eu/mithril/invoice/web/HomeController.java
@@ -5,7 +5,11 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import eu.mithril.invoice.web.forms.LoginForm;
 
 @Controller
 public class HomeController {
@@ -13,10 +17,22 @@ public class HomeController {
     @GetMapping("/")
     public String home(
             Model model,
-            @RequestParam("username") String username
+            @RequestParam(value = "username", required = false, defaultValue = "anon") String username
     ) {
         model.addAttribute("username", username);
         model.addAttribute("currentDate", LocalDateTime.now());
         return "index.html";
     }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm, Model model) {
+        return "redirect:/";
+    }
+
 }
